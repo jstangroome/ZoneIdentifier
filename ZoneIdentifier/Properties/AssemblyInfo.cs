fa41f13fd834df1c59f9b430e351135c2ec62e60
Jason Stangroome <jason@codeassassin.com>
2012-03-12 09:22:03 +1100

    Add Nuget packaging

--- ZoneIdentifier/Properties/AssemblyInfo.cs
+++ ZoneIdentifier/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CodeAssasssin.ZoneIdentifier")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Use the IZoneIdentifer API to unblock downloaded files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jason Stangroome")]
 [assembly: AssemblyProduct("CodeAssasssin.ZoneIdentifier")]
 [assembly: AssemblyCopyright("Copyright © Jason Stangroome 2012")]
 [assembly: AssemblyTrademark("")]
