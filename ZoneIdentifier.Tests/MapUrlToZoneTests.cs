d1f87a6707b65a68800381a77ff86021a1fc8398
Jason Stangroome <jason@codeassassin.com>
2012-03-12 17:45:32 +1100

    Add MapUrlToZone method

---
+++ ZoneIdentifier.Tests/MapUrlToZoneTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeAssassin.Tests
+{
+    [TestClass]
+    public class MapUrlToZoneTests
+    {
+        [TestMethod]
+        public void InternetSecurityManager_should_map_Microsoft_to_Internet_zone()
+        {
+            var zone = InternetSecurityManager.MapUrlToZone(@"http://www.microsoft.com/somepath");
+            Assert.AreEqual(UrlZone.Internet, zone);
+        }
+
+        [TestMethod]
+        public void InternetSecurityManager_should_map_Windows_Task_Manager_to_LocalMachine_zone()
+        {
+            var zone = InternetSecurityManager.MapUrlToZone(Path.Combine(Environment.SystemDirectory, "taskmgr.exe"));
+            Assert.AreEqual(UrlZone.LocalMachine, zone);
+        }
+
+    }
+}
---
+++ ZoneIdentifier/InternetSecurityManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace CodeAssassin
+{
+    public class InternetSecurityManager
+    {
+        private const string CLSID_InternetSecurityManager = "7b8a2d94-0ac9-11d1-896c-00c04fb6bfc4";
+
+        public static UrlZone MapUrlToZone(string url)
+        {
+            var ismType = Type.GetTypeFromCLSID(new Guid(CLSID_InternetSecurityManager));
+            var ism = (IInternetSecurityManager)Activator.CreateInstance(ismType);
+
+            uint zone;
+            ism.MapUrlToZone(url, out zone, 0);
+
+            return (UrlZone) zone;
+        }
+    }
+
+    [ComImport]
+    [Guid("79EAC9EE-BAF9-11CE-8C82-00AA004BA90B")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IInternetSecurityManager
+    {
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int SetSecuritySite([In] IntPtr pSite);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int GetSecuritySite([Out] IntPtr pSite);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int MapUrlToZone([In, MarshalAs(UnmanagedType.LPWStr)] string pwszUrl, [Out]out UInt32 pdwZone, UInt32 dwFlags);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int GetSecurityId([MarshalAs(UnmanagedType.LPWStr)] string pwszUrl,
+                  [MarshalAs(UnmanagedType.LPArray)] byte[] pbSecurityId,
+                  ref UInt32 pcbSecurityId, uint dwReserved);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int ProcessUrlAction([In, MarshalAs(UnmanagedType.LPWStr)] string pwszUrl,
+                     UInt32 dwAction, out byte pPolicy, UInt32 cbPolicy,
+                     byte pContext, UInt32 cbContext, UInt32 dwFlags,
+                     UInt32 dwReserved);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int QueryCustomPolicy([In, MarshalAs(UnmanagedType.LPWStr)] string pwszUrl,
+                  ref Guid guidKey, ref byte ppPolicy, ref UInt32 pcbPolicy,
+                  ref byte pContext, UInt32 cbContext, UInt32 dwReserved);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int SetZoneMapping(UInt32 dwZone,
+                   [In, MarshalAs(UnmanagedType.LPWStr)] string lpszPattern,
+                   UInt32 dwFlags);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int GetZoneMappings(UInt32 dwZone, out IEnumString ppenumString,
+                    UInt32 dwFlags);
+    }
+
+    
+}
